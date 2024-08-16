232dca286de20bfa440a11cb9907b6ddb5a067ab
Lucky10cr <lucky10cr@gmail.com>
2024-08-16 18:15:50 +0530

    SipoTest.v

--- Code/UART-Rx/SipoTest.v
+++ Code/UART-Rx/SipoTest.v
@@ -1,11 +1,3 @@
-//  AUTHOR: Mohamed Maged Elkholy.
-//  INFO.: Undergraduate ECE student, Alexandria university, Egypt.
-//  AUTHOR'S EMAIL: majiidd17@icloud.com
-//  FILE NAME: SipoTest.v
-//  TYPE: Test fixture "Test bench".
-//  DATE: 31/8/2022
-//  KEYWORDS: SIPO, Shift register, Reciever.
-
 `timescale 1ns/1ps
 module SipoTest;
 
