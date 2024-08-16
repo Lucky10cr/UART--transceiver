904e45e5050e7fa6b722fa4dabfa115d033774b7
Lucky10cr <lucky10cr@gmail.com>
2024-08-16 18:13:33 +0530

    DeFrame.v

--- Code/UART-Rx/DeFrame.v
+++ Code/UART-Rx/DeFrame.v
@@ -1,13 +1,3 @@
-//  AUTHOR: Mohamed Maged Elkholy.
-//  INFO.: Undergraduate ECE student, Alexandria university, Egypt.
-//  AUTHOR'S EMAIL: majiidd17@icloud.com
-//  FILE NAME: DeFrame.v
-//  TYPE: module.
-//  DATE: 31/8/2022
-//  KEYWORDS: Frame, Data.
-//  PURPOSE: An RTL modelling for separating the frame
-//  into Data bits, Parity bit, Start and Stop bits.
-
 module DeFrame(
     input wire  [10:0]  data_parll,     //  Data frame passed from the sipo unit.
     input wire          recieved_flag,
